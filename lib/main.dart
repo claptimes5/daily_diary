43da58362902f7f8dead346d58f834a63f6d9416
Avi Leavitt <avileavitt@gmail.com>
2020-02-19 12:02:39 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'recording_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),
+      home: RecordingScreen(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>(); // Add this line.
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[      // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+                trailing: Icon(
+                  Icons.delete
+                ),
+                onTap: () {
+                  setState(() {
+                    _saved.remove(pair);
+                  });
+                },
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),                       // ... to here.
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider();
+          /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(5)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
---
+++ lib/recording_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class RecordingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Record an Entry')),
+      body: _buildPage(),
+    );
+  }
+
+  Widget _buildPage() {
+    return Container(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          children: [titleSection, recordSection, playSection, saveSection],
+        ));
+  }
+
+  Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Record for today __',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+
+  Widget recordSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Column(children: [
+        Icon(Icons.fiber_manual_record, color: Colors.red, size: 100),
+        Text('15 Seconds')
+      ]));
+
+  Widget playSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Center(
+          child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+            Icon(Icons.fast_rewind, size: 70),
+            Icon(Icons.play_arrow, size: 70)
+          ])));
+
+  Widget saveSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: IconButton(icon: Icon(Icons.save), onPressed: () { },)
+  );
+}
